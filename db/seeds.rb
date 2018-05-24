e308470234bcd73deecdfa5d87ee34575f0ed6d3
JanMeckelholt <janmeckelholt@gmail.com>
2018-05-24 14:21:41 +0200

    Layout and jquery fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Category.create(name: "Sample Category 1A")
 Category.create(name: "Sample Category 2B")
 Category.create(name: "Sample Category 3C")
 
-Post.create(title: "Sample Post 1", content: "Sample Content 1....adadfadfkkkasdfkke", category_id: 1)
+Post.create(title: "Sample Post 1", content: "Sample Content 1....Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", category_id: 1)
 Post.create(title: "Sample Post 2", content: "Sample Content 2....adadfadfkkkasdfkke", category_id: 1)
 Post.create(title: "Sample Post 3", content: "Sample Content 3....adadfadfkkkasdfkke", category_id: 2)
 
